78ce4a06c2c99ef0b7946697f53e66803444acf1
vlad <myalkin.vlad@yandex.ru>
2024-06-10 15:05:21 +0300

    feat: Добавление debug_toolbar.

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies.apps.MoviesConfig',
+    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
@@ -54,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -72,6 +75,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "0.0.0.0",
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
