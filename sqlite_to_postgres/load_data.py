077c8d734ca465c56204c246f10c21da6b03bb7b
vlad <myalkin.vlad@yandex.ru>
2024-06-22 15:55:05 +0300

    refactor: Добавление контекстного менеджера для работы с sqlite.

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import sqlite3
 from dataclasses import fields, astuple
 from typing import List, Optional
-
+from contextlib import contextmanager
 import psycopg
 from psycopg import ClientCursor, connection as _connection
 from psycopg.rows import dict_row
@@ -20,6 +20,26 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w",
                     format="%(asctime)s %(levelname)s %(message)s")
+
+
+@contextmanager
+def open_db(file_name: str):
+    conn = sqlite3.connect(file_name)
+    try:
+        logging.info("Creating connection")
+        yield conn.cursor()
+    finally:
+        logging.info("Closing connection")
+        conn.commit()
+        conn.close()
+
+
+@contextmanager
+def closing(thing):
+    try:
+        yield thing
+    finally:
+        thing.close()
 
 
 class PostgresSaver:
@@ -78,9 +98,8 @@ class PostgresSaver:
 
 
 class SQLiteLoader:
-    def __init__(self, connection):
-        self.connection = connection
-        self.cursor = connection.cursor()
+    def __init__(self, cursor):
+        self.cursor = cursor
         self.tables = [('genre', GenreSchema),
                        ('person', PersonSchema),
                        ('film_work', FilmWorkSchema),
@@ -90,20 +109,21 @@ class SQLiteLoader:
 
     def load_movies(self) -> List[dict]:
         """Функция считывает данные из sqlite3."""
-        try:
-            data = list()
-            for table, schema in self.tables:
-                self.cursor.execute(f"SELECT * FROM {table}")
-                data_table = [schema(*row) for row in self.cursor.fetchall()]
-                data.append({'table': table, 'schema': schema, 'data': data_table})
-            return data
-        except sqlite3.IntegrityError:
-            logging.error("Ошибка при загрузке данных из sqlite.")
-        except Exception as e:
-            logging.error(e)
+        with closing(self.cursor) as cursor:
+            try:
+                data = list()
+                for table, schema in self.tables:
+                    cursor.execute(f"SELECT * FROM {table}")
+                    data_table = [schema(*row) for row in cursor.fetchall()]
+                    data.append({'table': table, 'schema': schema, 'data': data_table})
+                return data
+            except sqlite3.IntegrityError:
+                logging.error("Ошибка при загрузке данных из sqlite.")
+            except Exception as e:
+                logging.error(e)
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
+def load_from_sqlite(connection: sqlite3.Cursor, pg_conn: _connection):
     """Основной метод загрузки данных из SQLite в Postgres"""
     postgres_saver = PostgresSaver(pg_conn)
     sqlite_loader = SQLiteLoader(connection)
@@ -120,7 +140,7 @@ if __name__ == '__main__':
         'host': os.environ.get('HOST'),
         'port': os.environ.get('PORT')
     }
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg.connect(
+    with open_db(file_name='db.sqlite') as sqlite_cursor, psycopg.connect(
             **dsl, row_factory=dict_row, cursor_factory=ClientCursor
     ) as pg_conn:
-        load_from_sqlite(sqlite_conn, pg_conn)
+        load_from_sqlite(sqlite_cursor, pg_conn)
